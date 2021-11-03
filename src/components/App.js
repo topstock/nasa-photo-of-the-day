bd812fae34d63a58b098c516de77cbb77d39791e
Phil G <fillupgalbraith@gmail.com>
2021-11-03 14:52:22 -0700

    adding component files

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
+import Card from "Card";
+import SearchBar from "SearchBar";
+
 
 function App() {
   return (
     <div className="App">
+      <>
+      <div>
+        <h1>Space Photos Sourced from NASA</h1>
+        {/*<SearchBar />*/}
+      </div>
+        <Card />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      </>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,1 @@
+//  font-family: 'Libre Caslon Text', serif; for Article
